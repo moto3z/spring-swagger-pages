b1cf655048d5a80081637eb5a986e6eae6b835e9
kdh <ultraraptor@naver.com>
2023-08-11 16:45:11 +0900

    init

---
+++ src/test/java/com/example/springswaggerpages/SpringSwaggerPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springswaggerpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerPagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
